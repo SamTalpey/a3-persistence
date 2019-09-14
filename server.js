d645b84215b497deefe2c16f259987794032b572
Sam Talpey <sjtalpey@gmail.com>
2019-09-14 14:25:09 -0400

    starting point for glitch

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express'),
     server = express(),
     bodyparser = require('body-parser'),
-    dataStore = [];
+    dir = '/public/',
+    low = require('lowdb'),
+    FileSync = require('lowdb/adapters/FileSync'),
+    adapter = new FileSync('db.json'),
+    db = low(adapter);
+
+// Setting defaults for empty JSON file
+db.defaults({ posts: [], user: {}, count: 0})
+  .write()
+
+// Handling posts
+db.get('posts')
+  // TODO Add the actual data
+  .push({id: 1, title: 'lowdb works'})
+  .write()
 
 // Deliver all files to public folder
+// TODO this doesnt actually seem to do anything
 server.use(express.static('public'));
 
 // Get JSON when relevant
@@ -11,7 +26,7 @@ server.use(bodyparser.json());
 
 // Explicity handle domain name
 server.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + dir + '/views/index.html');
 })
 
 server.post('/submit', function(req, res) {
@@ -20,20 +35,4 @@ server.post('/submit', function(req, res) {
   response.end(JSON.stringify(request.json));
 })
 
-//server.use(function(req, res, next) {
-//  console.log('url:', req.url);
-//  let dataString = '';
-//
-//  request.on('data', function(data) {
-//    dataString += data;
-//  })
-//
-//  request.on('end', function() {
-//    const json = JSON.parse(dataString);
-//    dataStore.push(json);
-//    request.json = JSON.stringify(dataStore);
-//    next();
-//  })
-//})
-
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 3000);
